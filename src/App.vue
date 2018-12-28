cabaf42c26d74eb084d711e5df7ac24682e9b8b3
unknown <15901453613@163.com>
2018-12-28 19:37:27 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
         <Layout>
             <Header>
                 <Menu mode="horizontal" theme="dark" active-name="1">
-                    <div class="layout-logo"></div>
+                    <!-- logo页面 -->
+                    <div class="layout-logo">
+                        <img src="./assets/m_logo.png" alt="">
+                    </div>
+                    <!-- 头部 -->
                     <div class="layout-nav">
                         <MenuItem name="1">
                             <Icon type="ios-navigate"></Icon>
@@ -27,47 +31,48 @@
                 </Menu>
             </Header>
             <Layout>
-               <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
-            <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-                <Submenu name="1">
-                    <template slot="title">
-                        <Icon type="ios-navigate"></Icon>
-                        Item 1
-                    </template>
-                    <MenuItem name="home">
-                    Home
-                    </MenuItem>
-                    <MenuItem name="1-2">Option 2</MenuItem>
-                    <MenuItem name="1-3">Option 3</MenuItem>
-                </Submenu>
-                <Submenu name="2">
-                    <template slot="title">
-                        <Icon type="ios-keypad"></Icon>
-                        Item 2
-                    </template>
-                    <MenuItem name="2-1">Option 1</MenuItem>
-                    <MenuItem name="2-2">Option 2</MenuItem>
-                </Submenu>
-                <Submenu name="3">
-                    <template slot="title">
-                        <Icon type="ios-analytics"></Icon>
-                        Item 3
-                    </template>
-                    <MenuItem name="3-1">Option 1</MenuItem>
-                    <MenuItem name="3-2">Option 2</MenuItem>
-                </Submenu>
-            </Menu>
-        </Sider>
+                <!-- 侧边 -->
+                <Sider hide-trigger :style="{background: '#fff'}">
+                    <Menu active-name="1-1" theme="light"  width="auto" @on-select="goHome">
+                        <Submenu name="1">
+                            <template slot="title">
+                                <Icon type="ios-navigate"></Icon>
+                            信息管理
+                            </template>
+                            <MenuItem name="1">我的公司</MenuItem>
+                            <MenuItem name="2" >许可证管理</MenuItem>
+                            <MenuItem name="3">商标管理</MenuItem>
+                            <MenuItem name="4">网站备案管理</MenuItem>
+                        </Submenu>
+                        <Submenu name="2">
+                            <template slot="title">
+                                <Icon type="ios-keypad"></Icon>
+                                Item 2
+                            </template>
+                            <MenuItem name="2-1">Option 1</MenuItem>
+                            <MenuItem name="2-2">Option 2</MenuItem>
+                        </Submenu>
+                        <Submenu name="3">
+                            <template slot="title">
+                                <Icon type="ios-analytics"></Icon>
+                                Item 3
+                            </template>
+                            <MenuItem name="3-1">Option 1</MenuItem>
+                            <MenuItem name="3-2">Option 2</MenuItem>
+                        </Submenu>
+                    </Menu>
+                </Sider>
                 <Layout :style="{padding: '0 24px 24px'}">
                     <Breadcrumb :style="{margin: '24px 0'}">
                         <BreadcrumbItem>Home</BreadcrumbItem>
                         <BreadcrumbItem>Components</BreadcrumbItem>
                         <BreadcrumbItem>Layout</BreadcrumbItem>
                     </Breadcrumb>
-                    <Content :style="{padding: '24px', minHeight: '575px', background: '#fff'}">
-                        Content
+                    <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+                        <router-view></router-view>
                     </Content>
                 </Layout>
+                 
             </Layout>
         </Layout>
     </div>
@@ -78,9 +83,22 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import $router from "./router.js";
 
 export default {
   name: "app",
+  data() {
+    return {};
+  },
+  methods: {
+    goHome(name) {
+      this.$router.push({ path: "/About",name:"name" });
+    //   this.$router.push({ path: "/test",name:"name" });
+    },
+    // goIcp(name) {
+    //   this.$router.push({ path: "/test" });
+    // }
+  },
   components: {
     HelloWorld
   }
@@ -97,7 +115,6 @@ export default {
   /* margin-top: 60px; */
 }
 .layout {
-  border: 1px solid #d7dde4;
   background: #f5f7f9;
   position: relative;
   border-radius: 4px;
@@ -106,7 +123,6 @@ export default {
 .layout-logo {
   width: 100px;
   height: 30px;
-  background: #5b6270;
   border-radius: 3px;
   float: left;
   position: relative;
@@ -117,5 +133,37 @@ export default {
   width: 420px;
   margin: 0 auto;
   margin-right: 20px;
+}
+.layout-con {
+  height: 100%;
+  width: 100%;
+}
+.menu-item span {
+  display: inline-block;
+  overflow: hidden;
+  width: 69px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: bottom;
+  transition: width 0.2s ease 0.2s;
+}
+.menu-item i {
+  transform: translateX(0px);
+  transition: font-size 0.2s ease, transform 0.2s ease;
+  vertical-align: middle;
+  font-size: 16px;
+}
+.collapsed-menu span {
+  width: 0px;
+  transition: width 0.2s ease;
+}
+.collapsed-menu i {
+  transform: translateX(5px);
+  transition: font-size 0.2s ease 0.2s, transform 0.2s ease 0.2s;
+  vertical-align: middle;
+  font-size: 22px;
+}
+.ivu-layout.ivu-layout-has-sider {
+  min-height: 724px;
 }
 </style>
